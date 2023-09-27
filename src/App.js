2457e20f1151931b3cce82a7ad059a23ec10d6fc
ManmohanJangra25 <manmohanjangra2528@gmail.com>
2023-09-27 22:50:38 +0530

    Basic Connections done with useeffect and useCallback

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 
 import MoviesList from './components/MoviesList';
+import SpinLoader from './loading.gif';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [dummyMovies, setDummyMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMoviesHandler = useCallback(async() => {
+    setIsLoading(true);
+    try{
+      const response = await fetch('https://swapi.py4e.com/api/films/');
+      if(!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+      const transformedData = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          releaseDate: movieData.release_date,
+          openingText: movieData.opening_crawl
+        };
+      })
+      setTimeout(() => {
+        setDummyMovies(transformedData);
+        setIsLoading(false);
+      }, 2000);
+    } catch (error) {
+      alert(error.message);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {isLoading && <img src={SpinLoader} alt="loading" height="50" width="50" />}
+        {!isLoading && dummyMovies.length === 0 && <p>Found No Movies</p>}
+        {!isLoading && dummyMovies.length > 0 && <MoviesList movies={dummyMovies} />}
       </section>
     </React.Fragment>
   );
